e3e4fc1af4ecceec5725d0e42d20e05113b3fc52
Nicholas Zangara <zangaran1@newpaltz.edu>
2023-03-15 12:51:54 -0400

    user profile and bio

--- client/src/views/otherViews/userProfileView.vue
+++ client/src/views/otherViews/userProfileView.vue
@@ -7,17 +7,22 @@ const session = useSession();
 </script>
 
 <template>
-   <div class="user-top-banner">
+   <div class="username-banner">
         <div class="username" v-if="session.user">
             <p id="username-txt">
                 {{ session.user.name }}
             </p>
         </div>
+   </div>
+   <div class="user-top-banner">
         <div class="follower-count">
             Followers: 10
         </div>
         <div class="following-count">
             Following: 13
+        </div>
+        <div class="workouts-completed-count">
+            Workouts Completed: 5
         </div>
         <aside>
             <div class="user-profile-picture">
@@ -25,9 +30,56 @@ const session = useSession();
             </div>
         </aside>
    </div>
+   <div class="user-bio-container">
+        <div class="user-bio">
+            <p >
+                Hello My name is Nicholas Zangara, and I am a Senior Computer Science Student at SUNY New Paltz. I also play baseball for the school. I am a left handed pitcher, and fitness and athletics is a big part of my life!
+            </p>
+        </div>
+   </div>
 </template>
 
 
 <style scoped>
-
+#profile-pic{
+    border-radius: 75%;
+    width: 100px;
+    height: 100px;
+}
+.username-banner{
+    padding: 10px;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+}
+#username-txt{
+    color: white;
+    font-size: 30px;
+}
+.user-top-banner{
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    border-radius: 20px;
+    background-color: rgb(51, 49, 49);
+}
+.follower-count, .following-count, .workouts-completed-count{
+    font-size: larger;
+    padding: 20px;
+    color: white;
+   
+}
+.user-profile-picture{
+    padding: 20px;
+}
+.user-bio-container{
+    padding: 20px;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+}
+.user-bio{
+    padding: 20px;
+    color: white;
+}
 </style>
