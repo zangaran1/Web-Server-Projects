cfb1403645827bf8a727718038b0b21e12aec233
Nicholas Zangara <zangaran1@newpaltz.edu>
2023-03-19 20:06:36 -0400

    interface creation

--- client/src/views/navbarFolder/WorkoutsView.vue
+++ client/src/views/navbarFolder/WorkoutsView.vue
@@ -1,6 +1,38 @@
+<script setup lang="ts">
+
+</script>
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="section">
+    <div class="container">
+      <div class="start-a-workout-section">
+        <button class="button is-info is-large">Start A New Workout</button>
+      </div>
+      <div class="modal is-active">
+        <div class="modal-content">
+          <h1>Press + to add a Workout</h1>
+          <button class="button is-info">+</button>
+        </div>
+      </div>
+      <div class="workouts">
+      
+      </div>
+    </div>
   </div>
 </template>
 
+<style scoped>
+.section{
+  background-color: white;
+  margin-top: 20px;
+}
+.container{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+.start-a-workout-section{
+  padding: 25px;
+}
+</style>
