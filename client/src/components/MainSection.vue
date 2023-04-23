ab94368e316fb087c0f533e49f8b955588d16df7
Nicholas Zangara <zangaran1@newpaltz.edu>
2023-04-23 17:27:39 -0400

    progress bars

--- client/src/components/MainSection.vue
+++ client/src/components/MainSection.vue
@@ -9,12 +9,52 @@ const session = useSession();
 </script>
 
 <template>
-  <div class="card" id="card">
-
-    <div class="card-content">
-
+    <div class="section">
+        <div class="columns">
+            <div class="column is-half">
+                <div class="column">
+                    <div class="card">
+                        <div class="card-header">
+                            <p class="card-header-title">
+                                Weekly Progress
+                            </p>
+                        </div>
+                        <div class="card-content">
+                            <div class="content">
+                                <div class="progress">
+                                    <span>
+                                        <progress class="progress is-primary" value="15" max="100"></progress>
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="column is-half">
+                <div class="columns">
+                    <div class="column is-half">
+                        <div class="card">
+                            <div class="card-header">
+                                <p class="card-header-title">
+                                    Weekly Progress
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column is-half">
+                        <div class="card">
+                            <div class="card-header">
+                                <p class="card-header-title">
+                                    Weekly Progress
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-</div>
 </template>
 
 <style scoped>
@@ -22,9 +62,26 @@ const session = useSession();
         justify-content: center;
         background-color: hsl(348, 100%, 61%);
     }
-    #card{
-    background-color: rgb(44, 44, 44);
+    .card{
+        background-color: rgb(44, 44, 44);
     }
+    .card-header-title{
+        color: white;
+    }
+    .progress{
+        width: 150px;
+        height: 150px;
+        line-height: 150px;
+        background: none;
+        margin: 0 auto;
+        box-shadow: none;
+        position: relative;
+    }
+   
+
+
+    
+
     
 
 </style>
